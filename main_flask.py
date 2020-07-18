a21e872fb3ca93435d0b92d2f53f47dd495d4334
klq26-Ryzen2600 <klq26@163.com>
2020-07-18 17:45:40 +0800

    新增 uwsgi 配置文件和 nginx 代码举例。 新增单独发钉钉，发 email 的函数。函数解耦合。

--- main_flask.py
+++ main_flask.py
@@ -47,32 +47,82 @@ def packDataWithCommonInfo(isCache = False, isSuccess = True, msg = "success", d
     result = {'code' : code, 'msg' : msg, 'isCache' : False, 'aliyun_date' : dm.getDateTimeString(), 'data' : data, 'duration' : duration}
     return json.dumps(result, ensure_ascii=False, indent=4, sort_keys=True)
 
-@app.route('/message/api/dingtalk', methods=['POST'])
-def send_msg_by_dingtalk():
-    start_ts = dm.getTimeStamp()
-    # data = json.dumps(records, ensure_ascii=False, indent=4)
-    data = {}
-    if request.method=='POST':
-        class_name = request.form['class_name']
-        # eval 是从字符串字典转回 dict
-        obj = eval(request.form['data'])
-        if obj != None and len(class_name) > 0:
-            # 入数据库（确定 id）
-            newId = 1
-            # ★ 如果是初次创建数据库，select 会失败，此时应该注释下面 4 行代码，不做判断了，场景不多
+def add_to_db(req_form):
+    """
+    把 http request 中 POST 参数写入数据库，并返回消息软件需要的格式（title，text）
+    """
+    # 数据库表名
+    class_name = req_form['class_name']
+    # eval 是从字符串字典转回 dict
+    obj = eval(request.form['data'])
+    is_init = 0
+    if 'init' in request.form.keys():
+        is_init = int(req_form['init'])
+    if obj != None or len(class_name) > 0:
+        # 入数据库（确定 id）
+        newId = 1
+        tb_exist_operation = 'replace'
+        # 如果不是初次创建数据库，则需要查询最后一条入库的数据 id，以便决定新数据的 index
+        if not is_init or is_init == 0:
+            tb_exist_operation = 'append'
             sql = f"""SELECT * FROM {class_name} order by id DESC LIMIT 1"""
             latest = pd.read_sql(sql, engine)
             if len(latest.id.values) > 0:
                 newId = latest.id.values[0] + 1
-            obj['id'] = int(newId)
-            obj['aliyun_date'] = dm.getDateTimeString()
-            df = pd.DataFrame(obj, index=[newId])
-            df.to_sql(f'{class_name}', engine, if_exists='append', index=True)
-            # 发钉钉
-            dingdingMessager().send(f'{class_name}\n\n{json.dumps(obj,indent=4,sort_keys=False,ensure_ascii=False)}')
-            # # 发邮件
-            emailMessager().send(subject=f'{class_name}', msg=json.dumps(obj,indent=4,sort_keys=False, ensure_ascii=False))
+        obj['id'] = int(newId)
+        obj['aliyun_date'] = dm.getDateTimeString()
+        df = pd.DataFrame(obj, index=[newId])
+        df.to_sql(f'{class_name}', engine, if_exists=tb_exist_operation, index=True)
+        return {'title':class_name, 'text': json.dumps(obj, indent=4, sort_keys=False, ensure_ascii=False)}
 
+@app.route('/message/api/email', methods=['POST'])
+def send_msg_by_email():
+    """
+    接收 json 数据，写入数据库，并给管理员发邮件
+    """
+    start_ts = dm.getTimeStamp()
+    data = {}
+    if request.method=='POST':
+        # 插入数据库，返回数据
+        info = add_to_db(request.form)
+        # 发邮件
+        emailMessager().send(subject=info['title'], msg=info['text'])
+    end_ts = dm.getTimeStamp()
+    duration = dm.getDuration(start_ts, end_ts)
+    data = packDataWithCommonInfo(duration = duration, data = data)
+    return Response(data, status=200, mimetype='application/json')
+
+@app.route('/message/api/dingtalk', methods=['POST'])
+def send_msg_by_dingtalk():
+    """
+    接收 json 数据，写入数据库，并给管理员发钉钉消息
+    """
+    start_ts = dm.getTimeStamp()
+    data = {}
+    if request.method=='POST':
+        # 插入数据库，返回数据
+        info = add_to_db(request.form)
+        # 发钉钉
+        dingdingMessager().send(info['title'] + '\n\n' + info['text'])
+    end_ts = dm.getTimeStamp()
+    duration = dm.getDuration(start_ts, end_ts)
+    data = packDataWithCommonInfo(duration = duration, data = data)
+    return Response(data, status=200, mimetype='application/json')
+
+@app.route('/message/api/all', methods=['POST'])
+def send_msg_by_all_approach():
+    """
+    接收 json 数据，写入数据库，并通过所有途径通知管理员
+    """
+    start_ts = dm.getTimeStamp()
+    data = {}
+    if request.method=='POST':
+        # 插入数据库，返回数据
+        info = add_to_db(request.form)
+        # 发钉钉
+        dingdingMessager().send(info['title'] + '\n\n' + info['text'])
+        # 发邮件
+        emailMessager().send(subject=info['title'], msg=info['text'])
     end_ts = dm.getTimeStamp()
     duration = dm.getDuration(start_ts, end_ts)
     data = packDataWithCommonInfo(duration = duration, data = data)
--- post_request_test.py
+++ post_request_test.py
@@ -10,10 +10,10 @@ value_dict = {"id":0,"name":"李淑云", "lat": 123.3321, "lng": 59.2923}
 post_data={
     "data" : str(value_dict),
     "class_name": "location",
+    "init":0
 }
-# post_data = json.dumps(data,indent=4,ensure_ascii=False)
-# post_data = data
-# print(post_data)
-res=requests.post(url="http://127.0.0.1:5000/message/api/dingtalk", data=post_data)
+# res=requests.post(url="http://127.0.0.1:5000/message/api/dingtalk", data=post_data)
+res=requests.post(url="http://127.0.0.1:5000/message/api/all", data=post_data)
+# res=requests.post(url="http://127.0.0.1:5000/message/api/email", data=post_data)
 # res=requests.post(url="http://112.125.25.230/gohome/api/location", data=post_data)
-# print(res.text)
+print(res.text)
